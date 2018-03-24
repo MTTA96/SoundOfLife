ef07cedb82f7f655b29be101aa659a8ab1d70568
MTTA96 <mtta200396@gmail.com>
2018-03-24 15:51:37 +0700

    no message

--- SoundOfLife/laravel/routes/web.php
+++ SoundOfLife/laravel/routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Second Route method – Root URL with ID will match this method
+Route::get('ID/{id}',function($id){
+     echo 'ID: '.$id;
+ });
+    
+// Third Route method – Root URL with or without name will match this method
+ Route::get('/user/{name?}',function($name = 'Virat Gandhi'){
+    echo "Name: ".$name;
+});
